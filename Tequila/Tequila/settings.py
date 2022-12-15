105aeaed95de819f9e6083d2737bac3c2213a39d
tosantiago <santiago.lima22@gmail.com>
2022-12-14 22:06:59 -0300

    codigo con conexion a BD

--- Tequila/Tequila/settings.py
+++ Tequila/Tequila/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'libreria',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Tequila.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ong',
+        'USER': 'root',
+        'PASSWORD': 'buitre22',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
