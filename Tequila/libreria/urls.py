6dcbf453d2084b41ff606f81df28b2295c8c6cdc
tosantiago <santiago.lima22@gmail.com>
2022-12-16 21:54:14 -0300

    se agrego html y se llego a crear post y super user

--- Tequila/libreria/urls.py
+++ Tequila/libreria/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio.html'),
+    path('login.html', views.login, name='login'),
+    path('inicio.html', views.inicio, name='inicio.html'),
 ]
--- Tequila/libreria/views.py
+++ Tequila/libreria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from libreria import templates
 
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1> Bienvenido a ONG</h1>")
+     return render(request, 'inicio.html')
+def login(request):
+     return render(request, 'login.html')     
